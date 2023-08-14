03acb61b08ff35938961a793b2292c931708ce46
Diedrich Vorberg <diedrich@tux4web.de>
2023-08-14 18:03:57 +0200

    Added copyright header

--- wikklytext/to_html.py
+++ wikklytext/to_html.py
@@ -1,3 +1,23 @@
+"""
+wikklytext.parser.py: Base parser interface to
+wikklytext.lexer. Part of the WikklyText suite.
+
+Copyright (C) 2007,2008 Frank McIngvale
+Copyright (C) 2023 Diedrich Vorberg
+
+Contact: diedrich@tux4web.de
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+"""
+
 import sys, re, html
 from io import StringIO
 
