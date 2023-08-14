1e84161e46df5fccf20f65e8a05cbc678e274c4e
Diedrich Vorberg <diedrich@tux4web.de>
2023-08-14 13:45:33 +0200

    *** empty log message ***

--- examples/every_markup.py
+++ examples/every_markup.py
@@ -1,4 +1,20 @@
-from wikklytext import to_html
+from wikklytext import to_html, Context, Macro, MacroLibrary
+
+class RedMacro(Macro):
+    """
+    Example for a simple tag_param()-based macro.
+    """
+    name = "red"
+
+    def tag_params(self):
+        # Yeah, don’t do this. A professional will return
+        #
+        #    { "class": "red-macro" }
+        #
+        # and properly define span.red-macro in CSS.
+        # Also: A professional chooses a name based on function no just
+        # not “red”.
+        return { "style": "color: red; font-weight: bold" }
 
 def main():
     with open("every_markup.wikkly") as fp:
@@ -11,7 +27,8 @@ def main():
           '</head>',
           '<body>',
           sep="\n")
-    print(to_html(wikkly))
+    print(MacroLibrary.__init__)
+    print(to_html(wikkly, Context(MacroLibrary(RedMacro))))
     print('</body>', '</html>', sep="\n")
 
 
